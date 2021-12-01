6d14bf2f915dce5d0981ecf618008a95fc114c5e
NexVera <nexvera0105@gmail.com>
2021-12-01 18:48:09 +0800

    Setup project structure

--- app/src/main/java/com/example/lyricsfinder/presentation/MainActivity.kt
+++ app/src/main/java/com/example/lyricsfinder/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.lyricsfinder
+package com.example.lyricsfinder.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lyricsfinder.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
