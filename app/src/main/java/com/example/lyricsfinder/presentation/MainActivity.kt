b4aaeecee75c25baaf9f4e1193091a221361fdcb
NexVera <nexvera0105@gmail.com>
2021-12-06 15:00:15 +0800

    #15 - Remove Unnecessary Code

--- app/src/main/java/com/example/lyricsfinder/presentation/MainActivity.kt
+++ app/src/main/java/com/example/lyricsfinder/presentation/MainActivity.kt
@@ -2,25 +2,13 @@ package com.example.lyricsfinder.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.lyricsfinder.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment).apply {
-            this@MainActivity.navController = navController
-            setupActionBarWithNavController(navController)
-        }
     }
-
-    override fun onSupportNavigateUp(): Boolean = navController.navigateUp() || super.onSupportNavigateUp()
 }
